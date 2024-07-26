5307d1291f21d363807dd8689850c2cea9139bca
Kiran Bhat <bhatkiran74@gmail.com>
2024-07-26 23:51:55 +0530

    Refresh configurations at runtime using refresh actuator path

--- accounts/src/main/java/com/keto/accounts/utils/dto/AccountContactDetailDto.java
+++ accounts/src/main/java/com/keto/accounts/utils/dto/AccountContactDetailDto.java
@@ -1,16 +1,18 @@
 package com.keto.accounts.utils.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
 @ConfigurationProperties(prefix = "account")
-public record AccountContactDetailDto(
-        String message,
-        String name,
-        String company,
-        List<String> contact,
-        String city
-) {
-
+@Getter
+@Setter
+public class AccountContactDetailDto {
+    private String message;
+    private String name;
+    private String company;
+    private List<String> contact;
+    private String city;
 }
--- loans/src/main/java/com/keto/loans/dto/LoansContactDetailDto.java
+++ loans/src/main/java/com/keto/loans/dto/LoansContactDetailDto.java
@@ -1,16 +1,18 @@
 package com.keto.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
 @ConfigurationProperties(prefix = "loan")
-public record LoansContactDetailDto(
-        String message,
-        String name,
-        String company,
-        List<String> contact,
-        String city
-) {
-
+@Getter
+@Setter
+public class LoansContactDetailDto{
+    private String message;
+    private String name;
+    private String company;
+    private List<String> contact;
+    private String city;
 }
