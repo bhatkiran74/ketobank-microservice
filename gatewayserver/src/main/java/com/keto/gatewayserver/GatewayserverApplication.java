6ddf9f35cea6301f60867b4018763014676d6268
Kiransing Bhat <bhatkiran74@gmail.com>
2024-08-29 00:34:41 +0530

    added a response headers

--- gatewayserver/src/main/java/com/keto/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/keto/gatewayserver/GatewayserverApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class GatewayserverApplication {
 
@@ -19,15 +21,21 @@ public class GatewayserverApplication {
 		return routeLocatorBuilder.routes()
 				.route(p -> p
 						.path("/ketobank/accounts/**")
-						.filters( f -> f.rewritePath("/ketobank/accounts/(?<segment>.*)","/${segment}"))
+						.filters( f -> f.rewritePath("/ketobank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+						)
 						.uri("lb://ACCOUNTS"))
 				.route(p -> p
 						.path("/ketobank/cards/**")
-						.filters( f -> f.rewritePath("/ketobank/cards/(?<segment>.*)","/${segment}"))
+						.filters( f -> f.rewritePath("/ketobank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+						)
 						.uri("lb://CARDS"))
 				.route(p -> p
 						.path("/ketobank/loans/**")
-						.filters( f -> f.rewritePath("/ketobank/loans/(?<segment>.*)","/${segment}"))
+						.filters( f -> f.rewritePath("/ketobank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+						)
 						.uri("lb://LOANS"))
 				.build();
 	}
